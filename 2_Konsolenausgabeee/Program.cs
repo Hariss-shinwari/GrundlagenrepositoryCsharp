f8c594aa8dd78e4bea46e3a87b6e6a1ca2acddc6
Haris Shinwari <hshinwari848@gmail.com>
2024-10-10 14:26:43 +0200

    Projektdateien hinzufügen.

---
+++ 2_Konsolenausgabeee/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2_Konsolenausgabeee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fubar");
+        }
+    }
+}
